74aaf342d4ae77405da0641593066393b3f082b0
Pulkit Agarwal <pulkit.agarwal@kelltontech.com>
2023-06-12 07:28:10 +0530

    continue on UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { MainComponent } from './main/main.component';
+import { SlickCarouselModule } from 'ngx-slick-carousel';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    MainComponent
+    MainComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SlickCarouselModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,19 +1,123 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss'],
 })
 export class MainComponent {
+  constructor(){
+    this.selectedItem = this.productArray[0];
+  }
+  title = 'ngSlick';
+  selectedItem:any;
+
+  slides = [342, 453, 846, 855, 234, 564, 744, 243];
+
+  slideConfig = {
+    "slidesToShow": 3.2,
+    "dots": false,
+    "arrows": false,
+    "slidesToScroll": 3,
+    "infinite": true,
+    "responsive": [
+      {
+        breakpoint: 1624,
+        settings: {
+          slidesToShow: 3.2,
+
+        }
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 2,
+
+        }
+      }
+    ]
+  };
+
+  addBlur(index:any){
+    if(index >= this.productArray.length - 3){
+      return 'addBlur'
+    }
+    return ''
+
+  }
+
+  slideConfigModal = {
+    "slidesToShow": 2.2,
+    "dots": false,
+    "arrows": false,
+    "slidesToScroll": 3,
+    "infinite": true,
+    "responsive": [
+      {
+        breakpoint: 1624,
+        settings: {
+          slidesToShow: 2.2,
+
+        }
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 2,
+
+        }
+      }
+    ]
+  };
+
+
+
+  selectCard(item:{}){
+    this.selectedItem = item
+  }
+
+
   productArray = [
     {
       name: 'David N',
       distance: '4 kms from Brisbane City, QLD',
-      location: 'Location 1',
+      location: 'Queensland, QLD',
       rating: '4.4',
       quality: ['Licensed', 'ABN'],
-      metaData: true,
+      metaData: false,
       tags: [
         'Architectural Homes',
         'Commercial',
@@ -25,7 +129,7 @@ export class MainComponent {
     {
       name: 'Omar D',
       distance: '4 kms from Brisbane City, QLD',
-      location: 'Location 1',
+      location: 'Queensland, QLD',
       rating: '',
       quality: ['Licensed', 'ABN'],
       metaData: true,
@@ -39,11 +143,11 @@ export class MainComponent {
     },
     {
       name: 'Jacob J',
-      distance: 10,
-      location: '4 kms from New York, USA',
+      distance: '4 kms from Brisbane City, QLD',
+      location: 'Queensland, QLD',
       rating: '4.4',
       quality: ['Licensed', 'ABN'],
-      metaData: true,
+      metaData: false,
       tags: [
         'Architectural Homes',
         'Commercial',
@@ -54,11 +158,11 @@ export class MainComponent {
     },
     {
       name: 'Omar D',
-      distance: '4 kms from Brisbane City, QLD',
-      location: 'Location 1',
+      distance: '10 kms from London City, QLD',
+      location: 'NeW York, QLD',
       rating: '',
       quality: ['Licensed', 'ABN'],
-      metaData: true,
+      metaData: false,
       tags: [
         'Architectural Homes',
         'Commercial',
@@ -69,11 +173,11 @@ export class MainComponent {
     },
     {
       name: 'Jacob J',
-      distance: '4 kms from Brisbane City, QLD',
-      location: 'Location 1',
+      distance: '10 kms from London City, QLD',
+      location: 'NeW York, QLD',
       rating: '4.4',
       quality: ['Licensed', 'ABN'],
-      metaData: true,
+      metaData: false,
       tags: [
         'Architectural Homes',
         'Commercial',
@@ -85,7 +189,7 @@ export class MainComponent {
     {
       name: 'Jacob J',
       distance: '4 kms from Brisbane City, QLD',
-      location: 'Location 1',
+      location: 'Queensland, QLD',
       rating: '4.4',
       quality: ['Licensed', 'ABN'],
       metaData: true,
@@ -100,10 +204,10 @@ export class MainComponent {
     {
       name: 'Mike',
       distance: '4 kms from Brisbane City, QLD',
-      location: 'Location 1',
+      location: 'Queensland, QLD',
       rating: '4.4',
       quality: ['Licensed', 'ABN'],
-      metaData: true,
+      metaData: false,
       tags: [
         'Architectural Homes',
         'Commercial',
@@ -115,10 +219,10 @@ export class MainComponent {
     {
       name: 'Omar D',
       distance: '4 kms from Brisbane City, QLD',
-      location: 'Location 1',
+      location: 'Queensland, QLD',
       rating: '4.4',
       quality: ['Licensed', 'ABN'],
-      metaData: true,
+      metaData: false,
       tags: [
         'Architectural Homes',
         'Commercial',
@@ -130,7 +234,7 @@ export class MainComponent {
     {
       name: 'Pulkit',
       distance: '4 kms from Brisbane City, QLD',
-      location: 'Location 1',
+      location: 'Queensland, QLD',
       rating: '4.4',
       quality: ['Licensed', 'ABN'],
       metaData: true,
